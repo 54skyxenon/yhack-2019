c058ac4bade796d34f50a8556355dbae80e3a698
DLo930 <dlo2@andrew.cmu.edu>
2019-10-26 13:22:49 -0400

    Handle edge cases

--- adapt_csv.py
+++ adapt_csv.py
@@ -17,8 +17,12 @@ def parse_addr(addr):
         (suite_idx, _) = suite.span()
         addr = addr[:suite_idx + 1] + addr[suite_idx + 2:]
 
+    if 'PO BOX' in addr:
+        addr = addr[7:] + addr[:7]
+
     # Replace first space with |
-    addr = '|'.join(addr.split(' ', 1))
+    if re.search('[0-9]+', addr):
+        addr = '|'.join(addr.split(' ', 1))
     return addr.strip()
 
 
