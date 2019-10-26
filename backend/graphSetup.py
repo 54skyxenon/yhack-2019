ccae6a83fe3394852ec03714a7e3aac287b1fbd1
SGinovker <sginovke@edu.uwaterloo.ca>
2019-10-26 09:58:37 -0400

    query wip

--- backend/graphSetup.py
+++ backend/graphSetup.py
@@ -10,7 +10,16 @@ class loadData(object):
 
     @staticmethod
     def _load_data(tx, message):
-        dataQuery = "LOAD CSV WITH HEADERS FROM 'https://github.com/EDRInc/RaD-EdrCore-Public/blob/master/Datasets/OccupantAddresses_CT_NewHaven_2005.csv'"
-        result = tx.run("", message=message)
+        dataQuery = """
+        LOAD CSV WITH HEADERS FROM 'https://github.com/EDRInc/RaD-EdrCore-Public/blob/master/Datasets/OccupantAddresses_CT_NewHaven_2005.csv' 
+        LOAD CSV WITH HEADERS FROM 'https://github.com/EDRInc/RaD-EdrCore-Public/blob/master/Datasets/OccupantAddresses_CT_NewHaven_2010.csv' 
+        LOAD CSV WITH HEADERS FROM 'https://github.com/EDRInc/RaD-EdrCore-Public/blob/master/Datasets/OccupantAddresses_CT_NewHaven_2014.csv' """
+        result = tx.run(dataQuery, message=message)
 
         return result
+
+    @staticmethod
+    def _match_(tx, address):
+        # assumes address is in json format
+        matchQuery = """
+            MATCH (:State {})
