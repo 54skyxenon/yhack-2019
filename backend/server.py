b61190c2c8ddf1418c578c27c9e27f25d4a48d9b
DLo930 <dlo2@andrew.cmu.edu>
2019-10-26 14:58:24 -0400

    Add query route

--- backend/server.py
+++ backend/server.py
@@ -1,9 +1,56 @@
-from flask import Flask
+from flask import Flask, g, Response, request
+from neo4j import GraphDatabase
+
+FIELDS = ['Year', 'State', 'City', 'Street', 'HouseNumber']
+ABBRV = ['y', 's', 'c', 'st', 'h']
+LIMIT = 100
+
+# To be modified
+uri = 'bolt://localhost:7687'
+driver = GraphDatabase.driver(uri, auth=("neo4j", "password"))
+
 app = Flask(__name__)
+
+
+def get_db():
+    if not hasattr(g, 'neo4j_db'):
+        g.neo4j_db = driver.session()
+    return g.neo4j_db
+
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'neo4j_db'):
+        g.neo4j_db.close()
+
+
+def get_query(fields):
+    query_fields = []
+
+    for i in range(len(FIELDS)):
+        # Field names may be incorrect
+        tmp = f'({ABVRV[i]}:{FIELDS[i]}'
+        if fields[i] != '*':
+            tmp += f' {{{FIELDS[i]}: \"{fields[i]}\"}}'
+        tmp += ')'
+        query_fields.append(tmp)
+
+    query = '-[:CONTAINS]->'.join(query_fields)
+    query += ' RETURNS s.State'
+    for i in range(1, len(FIELDS)):
+        query += f', {ABBRV[i]}.{FIELDS[i]}'
+
+    query += ' LIMIT ' + LIMIT
+    print(query)
+    return query;
+
 
 @app.route('/query', methods=['POST'])
 def simple_query():
-    query = request.form['query']
-    year = request.form['year']
+    db = get_db()
 
-    return None
+    f = request.form
+    fields = [f['year'], f['state'], f['city'], f['street'], f['num']]
+    results = db.run(get_query(fields))
+
+    return results
