a884f8a482b58716c2fe82c2c1911ea44c97ca6c
Brandon Liang <54skyxenon@gmail.com>
2019-10-27 00:06:16 -0400

    Deleted bad code and refined CSS

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -5,54 +5,39 @@ import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 
 class App extends React.Component {
-  state = {
-    uploadPassed: false,
-    step1ButtonClicked: false,
-    step1passed: false,
-    step2ButtonClicked: false,
-    step2passed: false,
-    step3ButtonClicked: false,
-    step3passed: false,
-    step4ButtonClicked: false
-  };
-
-  setStep1Passed() {
-    this.setState({
-      step1passed: true
-    });
-  }
-
-  setStep2Passed() {
-    this.setState({
-      step2passed: true
-    });
-  }
-
-  setStep3Passed() {
-    this.setState({
-      step3passed: true
-    });
+  constructor(props) {
+    super(props);
+    this.state = {};
   }
 
   render() {
     return (
       <div
         style={{
-          padding: "5%",
+          padding: "5%"
         }}
       >
-        <Tabs>
-          <TabList style={{margin: 0}}>
+        <div id="navbar">
+          <img
+            src={require("../assets/logo.jpg")}
+            className="App-logo"
+            width={480}
+            height={270}
+            alt="Graph.srch logo"
+          />
+        </div>
+        <Tabs id="main-tab-container">
+          <TabList style={{ margin: 0 }}>
             <Tab>Address Matching</Tab>
             <Tab>Compare</Tab>
           </TabList>
           <div className="tabContent">
-          <TabPanel>
-            <Home />
-          </TabPanel>
-          <TabPanel>
-            <About />
-          </TabPanel>
+            <TabPanel>
+              <Home />
+            </TabPanel>
+            <TabPanel>
+              <About />
+            </TabPanel>
           </div>
         </Tabs>
       </div>
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -2,42 +2,51 @@ import React from "react";
 import { Form, Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-export default function Home() {
-  return (
-    <div className="App">
-      <h1>Sample Text</h1>
-      <Form style={{fontSize: '1.2rem'}}>
-        <Form.Group controlId="ControlSelect1">
-          <Form.Label>Pick a state</Form.Label>
-          <Form.Control as="select">
-            <option>None selected</option>
-            <option>Massachusetts</option>
-            <option>Connecticut</option>
-          </Form.Control>
-          <Form.Text className="text-muted">You need to pick a state before you pick a city.</Form.Text>
-        </Form.Group>
-        <Form.Group controlId="ControlSelect2">
-          <Form.Label>Pick a city</Form.Label>
-          <Form.Control as="select" disabled={true}>
-            <option>None selected</option>
-          </Form.Control>
-        </Form.Group>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Enter an address</Form.Label>
-          <Form.Control type="text" placeholder="555 Example St." />
-        </Form.Group>
-        <Form.Group controlId="ControlSelect3">
-          <Form.Label>Year</Form.Label>
-          <Form.Control as="select">
-            <option>2005</option>
-            <option>2010</option>
-            <option>2014</option>
-          </Form.Control>
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Move to Step 2
-        </Button>
-      </Form>
-    </div>
-  );
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Sample Text</h1>
+        <Form style={{ fontSize: "1.2rem" }}>
+          <Form.Group controlId="ControlSelect1">
+            <Form.Label>Pick a state</Form.Label>
+            <Form.Control as="select">
+              <option>None selected</option>
+              <option>Massachusetts</option>
+              <option>Connecticut</option>
+            </Form.Control>
+            <Form.Text className="text-muted">
+              You need to pick a state before you pick a city.
+            </Form.Text>
+          </Form.Group>
+          <Form.Group controlId="ControlSelect2">
+            <Form.Label>Pick a city</Form.Label>
+            <Form.Control as="select" disabled={true}>
+              <option>None selected</option>
+            </Form.Control>
+          </Form.Group>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Enter an address</Form.Label>
+            <Form.Control type="text" placeholder="555 Example St." />
+          </Form.Group>
+          <Form.Group controlId="ControlSelect3">
+            <Form.Label>Year</Form.Label>
+            <Form.Control as="select">
+              <option>2005</option>
+              <option>2010</option>
+              <option>2014</option>
+            </Form.Control>
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Move to Step 2
+          </Button>
+        </Form>
+      </div>
+    );
+  }
 }
